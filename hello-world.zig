fa998b9d4d415080384946706e42573f7aa14b33
Felix Orinda <forinda82@gmail.com>
2022-07-13 20:17:33 +0300

    Add vscode language server

--- hello-world.zig
+++ hello-world.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
-pub fn main() void {
-    std.io.getStdOut().writeAll(
+pub fn main() !void {
+   try std.io.getStdOut().writeAll(
         "Hello World!\n",
     ) catch unreachable;
 }
